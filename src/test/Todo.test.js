df3556d83842c0b806fe0e2a71e6da7974663abd
NicKAnderson038 <andersonnick038@gmail.com>
2019-02-03 22:25:45 -0800

    beforeEach shallow mounting helper method

--- src/test/Todo.test.js
+++ src/test/Todo.test.js
@@ -4,12 +4,17 @@ import Todo from '../components/Todo'
 import { shallow } from './enzyme.config'
 
 describe('Checking todo main component', ()=>{
-
+    let wrapper;
+    beforeEach(()=> wrapper = shallow(<Todo/>))
 
     it('testing if todo is loaded', ()=>{
-        const wrapper = shallow(<Todo/>)
         wrapper.instance()
-        
+        expect(wrapper).toHaveLength(1)
+    })
+
+    it('testing if todo is loaded', ()=>{
+        wrapper.instance()
+        console.log(wrapper)
         expect(wrapper.find('[data-test="input-box"]'))
     })
 
