7038b0a91b838e847fd38466b6ccad58ffc69c58
NicKAnderson038 <andersonnick038@gmail.com>
2019-02-04 20:39:02 -0800

    updated it() statements

--- src/test/Todo.test.js
+++ src/test/Todo.test.js
@@ -4,21 +4,21 @@ import Todo from '../components/Todo'
 import { shallow } from './enzyme.config'
 
 describe('Checking todo main component', ()=>{
-    
+
     let wrapper;
     beforeEach(()=> wrapper = shallow(<Todo/>))
 
-    it('testing if todo is loaded', ()=>{
+    it('should have loaded todo and checked with toHaveLength() method', ()=>{
         wrapper.instance()
         expect(wrapper).toHaveLength(1)
     })
 
-    it('testing if todo is loaded', ()=>{
+    it('should have loaded todo and checked with find() method', ()=>{
         wrapper.instance()
         expect(wrapper.find('[data-test="input-box"]'))
     })
 
-    it('focus was activated', ()=> {
+    it('should have css pseudo class focus() on ref this.inputBox', ()=> {
         const mockObj = { focus: jest.fn() };
         const inst = wrapper.instance();
         inst.inputBox = mockObj;
